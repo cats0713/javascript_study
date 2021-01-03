a8b8ba41fce4b6edb44618237ee6d2258600ac6a
cats0713 <cats0713@naver.com>
2021-01-03 14:59:00 +0900

    study

---
+++ todo.js
@@ -0,0 +1,11 @@
+const calculator = {
+    plus: function (a,b) {
+        return a+b;    
+    }
+    ,minus:function (a,b) {
+        return a-b;
+        
+    }
+}
+const calResult= calculator.minus(10,6);
+console.log(calResult)
