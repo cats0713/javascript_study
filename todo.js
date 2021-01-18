f2ce79801636b84dfe1ef31caff5323e3e1df2ec
cats0713 <cats0713@naver.com>
2021-01-18 15:23:52 +0900

    navbar,todolist

--- todo.js
+++ todo.js
@@ -4,22 +4,47 @@ const toDoForm = document.querySelector(".js-toDoForm"),
 
 const TODOS_LS = "todos";
 
+const toDos = [];
 
+function saveToDos(){
+    localStorage.setItem(TODOS_LS,JSON.stringify(toDos));
+}
 
 function paintToDos(text){
     const li = document.createElement("li");
     const delBtn = document.createElement("button");
     const span = document.createElement("span");
+    const newId = toDos.length + 1;
 
     delBtn.innerText = "x";
     span.innerHTML = text
     li.appendChild(span);
     li.appendChild(delBtn); 
+    li.id = newId;
     toDoList.appendChild(li);
-    delBtn.addEventListener("click",delBtnClick)
+    delBtn.addEventListener("click",deleteToDo)
+
+    const toDoObj = {
+        text: text,
+        id: newId
+
+    };
+    toDos.push(toDoObj);
+    saveToDos();
 }
 
-function delBtnClick(){
+function loadToDos() {
+    const loadedToDos = localStorage.getItem(TODOS_LS);
+    if(loadedToDos !== null){
+        const parsedToDos = JSON.parse(loadedToDos);
+        parsedToDos.forEach(function(toDo) {
+            paintToDos(toDo.text);
+        });
+    }
+}
+
+function deleteToDo(event){
+    
     
 }
 
@@ -30,12 +55,7 @@ function hendleSubmit(){
     toDoInput.value = "";
 }
 
-function loadToDos() {
-    const toDos = localStorage.getItem(TODOS_LS);
-    if(toDos !== null){
 
-    }
-}
 
 function init(){
     loadToDos();
